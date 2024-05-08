a80935626dab86c3972a13875000982722b8cf28
Alejandro Arias <al3jandrorar@gmail.com>
2024-05-07 21:17:04 -0600

    Update and rename Calc3A.cpp to Calc3A_completa.cpp
    Cambio de nombre para mejor descripción.
    Añadidos comentarios que expliquen mas a fondo el código.

--- Calc3A_completa.cpp
+++ Calc3A_completa.cpp
@@ -1,14 +1,21 @@
+/* El siguiente código hace uso del condicional If-else para llevar a cabo las operaciones basicas
+de acuerdo a un numero que introduzcamos.
+1 la operacion sera suma.
+2 la operación sera resta.
+3 la operación sera multiplicación.
+4 la operación sera división. */
 #include <iostream>
 using namespace std;
 
 int main(){
-    int Op;
-    double N1, N2, Sum, Rest, Mult, Div;
+    int Op; // variable que guarda la eleccion de operacion que haremos.
+    double N1, N2; // las variables N1 y N2 son los 2 numeros que seran ingresados via tecleado.
+    double Sum, Rest, Mult, Div;  // Estas variables guardaran la operación correspondiente.
     cout << "ingrese su primer numero: " << endl;
     cin >> N1;
     cout << "ingrese su segundo numero: " << endl;
     cin >> N2;
-    cout << "Seleccione la operacion a realizar, mediante el numero." << endl;
+    cout << "Seleccione la operacion a realizar, introduzca el numero correspondiente a la operacion." << endl;
     cout << "1.- Sumar." << endl;
     cout << "2.- Restar." << endl;
     cout << "3.- Multiplicar." << endl;
@@ -29,6 +36,8 @@ int main(){
     else if(Op == 4){
         Div = N1 / N2;
         cout << "La Division es igual a :" << Div << endl;
+    } else {
+        cout << "No existe ninguna operacion relacionada a ese numero" <<endl;
     }
     system("pause");
     return 0;
